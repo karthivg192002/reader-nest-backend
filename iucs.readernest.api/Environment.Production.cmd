2b0ab097c6f41e6a090fb5c9893872a95dff42aa
Karthikeyan <karthikeyanvg19@gmail.com>
2026-07-09 22:29:01 +0530

    removed

--- iucs.readernest.api/Environment.Production.cmd
+++
@@ -1,4 +0,0 @@
-REM ================================
-REM LMS DATABASE
-REM ================================
-SETX READERNESTDESIGN "Host=5.223.64.206;Port=5432;Database=readernest;Username=admin;Password=ScaleLite2026XkP9mNqR"
--- iucs.readernest.api/Environment.Uat.cmd
+++
@@ -1,4 +0,0 @@
-REM ================================
-REM LMS DATABASE
-REM ================================
-SETX READERNESTDESIGN "Host=204.168.165.148;Port=5432;Database=readernest;Username=admin;Password=Password2026"
