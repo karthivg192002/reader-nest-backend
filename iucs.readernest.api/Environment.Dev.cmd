b2281cd8acbd4cb75cae4d3a7b93eb6930d0a15a
DeepakVijayasarathi <draj58294@gmail.com>
2026-07-19 08:49:37 +0530

    db changes

--- iucs.readernest.api/Environment.Dev.cmd
+++ iucs.readernest.api/Environment.Dev.cmd
@@ -1,4 +1,4 @@
 REM ================================
 REM LMS DATABASE
 REM ================================
-SETX READERNESTDESIGN "Host=localhost;Port=5432;Database=readernest;Username=postgres;Password=pass@123"
+SETX READERNESTDESIGN "Host=93.127.194.128;Port=5432;Database=reader_nest;Username=postgres;Password=Deepakiucs@2025"
